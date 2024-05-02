c4ba866251f7978581b105ae8a9de8d5a775d79a
SamuGasto <samuel.55321@gmail.com>
2024-05-02 01:05:53 -0400

    [Dev] Comenzando modulo base de datos

---
+++ ProyectoIngSoftware/DataBase.py
@@ -0,0 +1,62 @@
+import pyodbc
+import pandas as pd
+
+server = 'LAPTOP-KHL9HM6I\INGDESOFT'
+bd = 'master'
+user = 'ingDeSoft02024'
+password = 'Panconqueso'
+
+conn = 0
+cursor = 0
+
+def ConectarBaseDeDatos():
+    try:
+        global conn, cursor
+        connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={bd};UID={user};PWD={password}'
+        conn = pyodbc.connect(connectionString) 
+        cursor = conn.cursor()
+        print('Conexión exitosa')
+    except:
+        print('Error al conectar')
+
+def CerrarCursor():
+    global cursor
+    cursor.close()
+
+def ListaTablas():
+    sqlQuery = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';"
+    rows = cursor.execute(sqlQuery).fetchall()
+    for r in rows:
+        print(r)
+    CerrarCursor()
+
+def AgregarUsuario(email,password):
+    global conn, cursor
+    sqlQuery = '''
+    INSERT INTO Users 
+    VALUES (1, 'samuel.55321@gmail.com', 'tomate';)
+    '''
+    cursor.execute(sqlQuery)
+    conn.commit()
+    CerrarCursor()
+
+def EliminarUsuarios():
+    global conn, cursor
+    sqlQuery = '''
+    DELETE FROM Users;
+    '''
+    cursor.execute(sqlQuery)
+    conn.commit()
+    CerrarCursor()
+
+def ListaUsuarios():
+    global cursor
+    sqlQuery = '''
+    SELECT * FROM Users;
+    '''
+    rows = cursor.execute(sqlQuery).fetchall()
+    return rows
+
+
+
+
--- ProyectoIngSoftware/Main.py
+++ ProyectoIngSoftware/Main.py
@@ -1,15 +1,22 @@
 import customtkinter
+import DataBase as db
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("400x150")
 
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button = customtkinter.CTkButton(self, text="Conectar con base de Datos", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+
+        self.button = customtkinter.CTkButton(self, text="Mostrar tablas", command=self.ImprimirTablas)
         self.button.pack(padx=20, pady=20)
 
     def button_callbck(self):
-        print("button clicked")
+        db.ConectarBaseDeDatos()
+
+    def ImprimirTablas(self):
+        print(db.ListaTablas())
 
 app = App()
 app.mainloop()
