bff22ceb1b01bfab7c1c7a19ab2c78f9f851632e
SamuGasto <samuel.55321@gmail.com>
2024-06-10 20:19:32 -0400

    [DEV] Estilos

---
+++ ProyectoIngSoftware/App/Clases/Componentes/Estilos.py
@@ -0,0 +1,67 @@
+
+colores = {
+    #Mas oscuro a mas claro
+    "Primary" : ["#572020","#763226","#a2502f","#da7c39","#ffb943"],
+    "Gray" : ["#151515","#787878","#a8a8a8","#cdcdcd","#ededed"],
+    "Green": ["#132c13","#1e7625","#26a131","#2cc23b","#32df43"],
+    "Yellow": ["#2c2713","#766c1e","#a19426","#c2b32c","#dfce32"],
+    "Red" : ["#2c1313","#761e1e","#a12626","#c22c2c","#df3232"],
+    "Blue" : ["#001023","#003253","#005c8d","#008fcf","#00cdff"],
+
+}
+
+background = "#151515"
+
+buttonPrincipal = {
+    "text_color" : colores["Gray"][4],
+    "fg_color" : colores["Blue"][2],
+    "font" : ("Segoe UI", -15, "bold"),
+    "corner_radius" : 12,
+    "hover_color" : colores["Blue"][1],
+}
+
+botonGrande = {
+    "text_color" : colores["Gray"][4],
+    "fg_color" : colores["Primary"][2],
+    "font" : ("Segoe UI", -26, "bold"),
+    "corner_radius" : 12,
+    "hover_color" : colores["Primary"][1]
+}
+
+buttonSecondary = {
+    "text_color" : colores["Gray"][4],
+    "fg_color" : colores["Gray"][1],
+    "font" : ("Segoe UI", -15, "bold"),
+    "corner_radius" : 12
+}
+
+MegaTitulo = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -60, "bold"),
+}
+
+Titulo = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -40, "bold"),
+}
+
+Subtitulo = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -32),
+}
+
+Texto = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -20)
+}
+
+TextoItalica = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -20, "italic")
+}
+
+entryNormal = {
+    "text_color" : colores["Gray"][4],
+    "font" : ("Segoe UI", -13),
+    "corner_radius" : 6
+}
